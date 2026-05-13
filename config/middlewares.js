bd9cfc66c528512baf122ec9c2d4497bb1433cdd
Richard Stephenson <richard@bolser.co.uk>
2026-05-13 14:09:51 +0100

    Adds Cloudinary

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,7 +1,36 @@
 module.exports = [
   'strapi::logger',
   'strapi::errors',
-  'strapi::security',
+  // CSP exception: allow Cloudinary domains as image/media sources so the admin
+  // Media Library can render thumbnails of files served from res.cloudinary.com.
+  // Without this, images appear broken in the admin UI even though delivery to
+  // the public frontend works fine.
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'market-assets.strapi.io',
+            'res.cloudinary.com',
+          ],
+          'media-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'market-assets.strapi.io',
+            'res.cloudinary.com',
+          ],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::query',
